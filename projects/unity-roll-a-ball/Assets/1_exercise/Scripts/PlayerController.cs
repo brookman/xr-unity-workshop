69eea9acfb9d6cab410bef1256639c8a2d5e114e
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2018-04-25 22:19:28 +0200

    Add roll-a-ball project.

---
+++ projects/unity-roll-a-ball/Assets/1_exercise/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    //public float speed = 1;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(moveHorizontal, 0, moveVertical);
+
+        //force *= speed;
+
+        rb.AddForce(force);
+    }
+
+    /*void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }*/
+}
---
+++ projects/unity-roll-a-ball/Assets/1_exercise/Scripts/PlayerController2.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(force * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    private void setCountText()
+    {
+        countText.text = "Count: " + count;
+        if (count >= 8)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
